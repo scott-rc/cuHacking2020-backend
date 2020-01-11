584925f008be8f332ee8aa56f6a4f37e10606a91
Scott Côtê <scrcote@gmail.com>
2020-01-11 12:19:33 -0500

    initial commit

---
+++ src/event.ts
@@ -0,0 +1,8 @@
+enum Event {
+  createdTask = "created",
+  deletedTask = "deleted",
+  renamedTask = "renamed",
+  resetBoard = "reset"
+}
+
+export default Event;
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import http from "http";
+import socketio from "socket.io";
+import uuid from "uuid";
+import event from "./event";
+import { User } from "./types";
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+let users: User[] = [];
+
+io.on("connection", socket => {
+  console.log("user connected");
+  users.push({ uuid: uuid.v4(), socketId: socket.id });
+
+  socket.on(event.createdTask, message => {
+    console.log(`received "${event.createdTask}":`, message);
+  });
+
+  socket.on(event.deletedTask, message => {
+    console.log(`received "${event.deletedTask}"`, message);
+  });
+
+  socket.on(event.renamedTask, message => {
+    console.log(`received "${event.renamedTask}"`, message);
+  });
+
+  socket.on(event.resetBoard, message => {
+    console.log(`received "${event.resetBoard}"`, message);
+  });
+
+  socket.on("disconnected", () => {
+    console.log("user disconnected");
+    users = users.filter(u => u.socketId == socket.id);
+  });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`listenting on port ${port}`);
+});
