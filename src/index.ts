722fad281776dd85926f7a43c40f9d92d698bf49
Scott Côtê <scrcote@gmail.com>
2020-01-11 19:29:04 -0500

    trying to prevent memory leak

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,24 @@ import http from "http";
 import logger from "./lib/logger";
 import * as middleware from "./lib/middleware";
 import router from "./lib/router";
+import state from "./lib/state";
 import websocket from "./lib/websocket";
 
 const app = middleware.afterRoutes(router(middleware.beforeRoutes(express())));
 const server = websocket(http.createServer(app));
+
+setInterval(() => {
+  logger.beginDebug("checking if any sessions are invalid...");
+
+  for (let i = 0; i < state.length; i++) {
+    if (state[i].ws.OPEN === 3) {
+      logger.continueDebug("deleting closed session: %s", state[i].id);
+      delete state[i];
+    }
+  }
+
+  logger.continueDebug("finished checking sessions");
+}, 5000);
 
 server.listen(process.env.PORT, () => {
   logger.info(`listenting on port ${process.env.PORT}`);
